2fec0a7c64b63d08458e2d59b9e240f29b91519d
kitatas <315kitatas10703@gmail.com>
2024-11-21 00:34:38 +0900

    fix: BaseModalView

--- Assets/GameOff2024/Scripts/Common/Presentation/View/Modal/BaseModalView.cs
+++ Assets/GameOff2024/Scripts/Common/Presentation/View/Modal/BaseModalView.cs
@@ -13,14 +13,14 @@ namespace GameOff2024.Common.Presentation.View.Modal
             await HideAsync(0.0f, token);
         }
 
-        protected virtual async UniTask ShowAsync(float duration, CancellationToken token)
+        public virtual async UniTask ShowAsync(float duration, CancellationToken token)
         {
             canvasGroup.alpha = 1.0f;
             canvasGroup.blocksRaycasts = true;
             await UniTask.Yield(token);
         }
 
-        protected virtual async UniTask HideAsync(float duration, CancellationToken token)
+        public virtual async UniTask HideAsync(float duration, CancellationToken token)
         {
             canvasGroup.alpha = 0.0f;
             canvasGroup.blocksRaycasts = false;
