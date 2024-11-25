4e497f4984e4eb707640f8015fcf180c2c57c228
kitatas <315kitatas10703@gmail.com>
2024-11-25 14:54:23 +0900

    feat: Game Sceneへの遷移

--- Assets/GameOff2024/Scripts/Boot/Presentation/State/CheckState.cs
+++ Assets/GameOff2024/Scripts/Boot/Presentation/State/CheckState.cs
@@ -1,10 +1,19 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using GameOff2024.Common;
+using GameOff2024.Common.Domain.UseCase;
 
 namespace GameOff2024.Boot.Presentation.State
 {
     public sealed class CheckState : BaseState
     {
+        private readonly SceneUseCase _sceneUseCase;
+
+        public CheckState(SceneUseCase sceneUseCase)
+        {
+            _sceneUseCase = sceneUseCase;
+        }
+
         public override BootState state => BootState.Check;
 
         public override async UniTask InitAsync(CancellationToken token)
@@ -15,6 +24,9 @@ namespace GameOff2024.Boot.Presentation.State
         public override async UniTask<BootState> TickAsync(CancellationToken token)
         {
             await UniTask.Yield(token);
+
+            _sceneUseCase.Load(SceneName.Game);
+
             return BootState.None;
         }
     }
