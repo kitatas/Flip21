ba5be3e8935b38e3b7aaf331c800bfe56552e0f9
kitatas <315kitatas10703@gmail.com>
2024-12-02 00:44:57 +0900

    feat: BGMの再生

--- Assets/GameOff2024/Scripts/Boot/Presentation/State/LoadState.cs
+++ Assets/GameOff2024/Scripts/Boot/Presentation/State/LoadState.cs
@@ -1,14 +1,25 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using GameOff2024.Common;
+using GameOff2024.Common.Domain.UseCase;
 
 namespace GameOff2024.Boot.Presentation.State
 {
     public sealed class LoadState : BaseState
     {
+        private readonly SoundUseCase _soundUseCase;
+
+        public LoadState(SoundUseCase soundUseCase)
+        {
+            _soundUseCase = soundUseCase;
+        }
+
         public override BootState state => BootState.Load;
 
         public override async UniTask InitAsync(CancellationToken token)
         {
+            _soundUseCase.PlayBgm(Bgm.Main);
+
             await UniTask.Yield(token);
         }
 
