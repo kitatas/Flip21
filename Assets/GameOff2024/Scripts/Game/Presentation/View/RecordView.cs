e60c51348b62f55f6a5eb3a8914c27d31efe63b5
kitatas <315kitatas10703@gmail.com>
2024-12-02 01:40:24 +0900

    feat: 自身の記録をハイライト

--- Assets/GameOff2024/Scripts/Game/Presentation/View/RecordView.cs
+++ Assets/GameOff2024/Scripts/Game/Presentation/View/RecordView.cs
@@ -1,17 +1,24 @@
 using GameOff2024.Common;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GameOff2024.Game.Presentation.View
 {
     public sealed class RecordView : MonoBehaviour
     {
+        [SerializeField] private Image background = default;
         [SerializeField] private TextMeshProUGUI rank = default;
         [SerializeField] private TextMeshProUGUI userName = default;
         [SerializeField] private TextMeshProUGUI score = default;
 
         public void Render(RankingVO rankingVO)
         {
+            if (rankingVO.isSelf)
+            {
+                background.color = new Color(1.0f, 0.5f, 0.5f, 1.0f);
+            }
+
             rank.text = $"{rankingVO.rank}";
             userName.text = $"{rankingVO.name}";
             score.text = $"{rankingVO.score}";
